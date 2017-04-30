518ad114d11dd234b354a2cd426a721e6bc90519
Banyapon Poolsawas <banyapon.poo@dpu.ac.th>
2017-04-30 21:52:10 +0700

    SQL CI

--- test.php
+++ test.php
@@ -5,13 +5,21 @@ $user = "ibsdwuiafhlutf";
 $password = "hYAx_eEg7cFqclK0RXacAzV2FG";
 $port = "5432";
 
-$dsn = "pgsql:host=$host;dbname=$dbname;user=$user;port=$port;password=$password";
-
-$db = new PDO($dsn);
-
-if($db){
-  echo "Connected <br />".$db;
-}else {
-  echo "Not connected";
+# This function reads your DATABASE_URL configuration automatically set by Heroku
+# the return value is a string that will work with pg_connect
+function pg_connection_string() {
+  return "dbname=".$dbname." host=".$host." port=5432 user=".$user." password=".$password." sslmode=require";
 }
+ 
+# Establish db connection
+$db = pg_connect(pg_connection_string());
+if (!$db) {
+    echo "Database connection error."
+    exit;
+}else{
+   echo "Connected";
+}
+ 
+$result = pg_query($db, "CREATE TABLE duck ( id SERIAL, question VARCHAR(255), answer VARCHAR(255));");
+$result = pg_query($db, "INSERT INTO duck (question, answer) VALUES ('หวัดดี', 'ดีก๊าบบ');");
 ?>
