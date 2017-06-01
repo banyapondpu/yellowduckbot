7716d010b9635f4e9bdff134b2e3865cd4666a73
Banyapon Poolsawas <banyapon.poo@dpu.ac.th>
2017-06-01 14:21:32 +0700

    API BOT the 2nd

---
+++ api2.php
@@ -0,0 +1,97 @@
+<?php
+$strAccessToken = "10ZCualI1XhwRMLT9izFg/AjEzXrzLfaBMZgSo8ZCM61pgfREtjznKphChJGoVt+2ne+UYm6kiRhuISoFhbmzyn2SRVz6LllLwuj4muKuYpRpmwzZAW8q1pkqANUmzVrmikjxDb0vmswfHeqdZ9A7wdB04t89/1O/w1cDnyilFU=";
+$content = file_get_contents('php://input');
+$arrJson = json_decode($content, true);
+$strUrl = "https://api.line.me/v2/bot/message/reply";
+$arrHeader = array();
+$arrHeader[] = "Content-Type: application/json";
+$arrHeader[] = "Authorization: Bearer {$strAccessToken}";
+$_msg = $arrJson['events'][0]['message']['text'];
+$api_key="WMuixWmGz9G8FwjQFQ0lu0dzsaXxvo23";
+$url = 'https://api.mlab.com/api/1/databases/duckduck/collections/linebot?apiKey='.$api_key.'';
+$json = file_get_contents('https://api.mlab.com/api/1/databases/duckduck/collections/linebot?apiKey='.$api_key.'&q={"question":"'.$_msg.'"}');
+$data = json_decode($json);
+$isData=sizeof($data);
+if (strpos($_msg, 'สอนเป็ด') !== false) {
+  if (strpos($_msg, 'สอนเป็ด') !== false) {
+    $x_tra = str_replace("สอนเป็ด","", $_msg);
+    $pieces = explode("|", $x_tra);
+    $_question=str_replace("[","",$pieces[0]);
+    $_answer=str_replace("]","",$pieces[1]);
+    //Post New Data
+    $newData = json_encode(
+      array(
+        'question' => $_question,
+        'answer'=> $_answer
+      )
+    );
+    $opts = array(
+      'http' => array(
+          'method' => "POST",
+          'header' => "Content-type: application/json",
+          'content' => $newData
+       )
+    );
+    $context = stream_context_create($opts);
+    $returnValue = file_get_contents($url,false,$context);
+    $arrPostData = array();
+    $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+    $arrPostData['messages'][0]['type'] = "text";
+    $arrPostData['messages'][0]['text'] = 'ขอบคุณที่สอนเป็ด';
+  }
+}else{
+  if($isData >0){
+   foreach($data as $rec){
+    $arrPostData = array();
+    $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+    $arrPostData['messages'][0]['type'] = "text";
+    $arrPostData['messages'][0]['text'] = $rec->answer;
+   }
+  }else{
+    $arrPostData = array();
+    $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+    $arrPostData['messages'][0]['type'] = "text";
+    $arrPostData['messages'][0]['text'] = 'ก๊าบบ คุณสามารถสอนให้ฉลาดได้เพียงพิมพ์: สอนเป็ด[คำถาม|คำตอบ]';
+  }
+}
+if (strpos($_msg, ':)') !== false) {
+    $arrPostData = array();
+    $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+    $arrPostData['messages'][0]['type'] = "sticker";
+    $arrPostData['messages'][0]['packageId'] = '2';
+    $arrPostData['messages'][0]['stickerId'] = '22';
+}
+if (strpos($_msg, 'หาที่เรียน') !== false) {
+    $arrPostData = array();
+    $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+    $arrPostData['messages'][0]['type'] = "image";
+    $arrPostData['messages'][0]['originalContentUrl'] = 'https://firebasestorage.googleapis.com/v0/b/fir-login-cd219.appspot.com/o/FacebookAds3.png?alt=media&token=d5946950-e0ad-4ac3-be79-eaac8ce54df6';
+    $arrPostData['messages'][0]['previewImageUrl'] = 'https://firebasestorage.googleapis.com/v0/b/fir-login-cd219.appspot.com/o/FacebookAds3.png?alt=media&token=d5946950-e0ad-4ac3-be79-eaac8ce54df6';
+}
+if (strpos($_msg, 'ขอเสียง') !== false) {
+    $arrPostData = array();
+    $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+    $arrPostData['messages'][0]['type'] = "audio";
+    $arrPostData['messages'][0]['originalContentUrl'] = 'https://firebasestorage.googleapis.com/v0/b/fir-login-cd219.appspot.com/o/Duck-quack-sound.mp3?alt=media&token=6ee2c808-7caf-4519-92ac-a0ac18d1f384';
+    $arrPostData['messages'][0]['duration'] = '5000';
+}
+if (strpos($_msg, 'อยู่ไหน') !== false) {
+    $arrPostData = array();
+    $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+    $arrPostData['messages'][0]['type'] = "location";
+    $arrPostData['messages'][0]['title'] = "มหาวิทยาลัยธุรกิจบัณฑิตย์";
+    $arrPostData['messages'][0]['address'] = '110/1-4 ถนนประชาชื่น หลักสี่ กทม 10210';
+    $arrPostData['messages'][0]['latitude'] = '13.8703032';
+    $arrPostData['messages'][0]['longitude'] = '100.548575';
+}
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL,$strUrl);
+curl_setopt($ch, CURLOPT_HEADER, false);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $arrHeader);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrPostData));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+$result = curl_exec($ch);
+curl_close ($ch);
+?>
