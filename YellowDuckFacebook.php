3342d9042e69aaf645c8ab82f6b626058c47e7dc
Banyapon Poolsawas <banyapon.poo@dpu.ac.th>
2018-06-19 11:18:58 +0700

    Update YellowDuckFacebook.php

--- YellowDuckFacebook.php
+++ YellowDuckFacebook.php
@@ -1,96 +1,57 @@
+<html>
+<head>
+    <title>Analyze Image Sample</title>
+</head>
+<body>
 <?php
-$access_token = "EAAI3Y9A0cSIBANiqrqxLkSyxQgp7ZAvep13JEZC8eQNH9euvePhZB7Xsk6er7sMSQ9DIWGO8H05YewNS2AfyQmhUVhxa5LFIXDJoEDUxM9LCj27OiIN7EsBmvRxKw3tENT0bXa6On2d2CZBty7YWxX9TESa41HyCVy87tpeAfwZDZD";
-$verify_token = "yellow_duck";
-$hub_verify_token = null;
-if(isset($_REQUEST['hub_challenge'])) {
- $challenge = $_REQUEST['hub_challenge'];
- $hub_verify_token = $_REQUEST['hub_verify_token'];
-}
-if ($hub_verify_token === $verify_token) {
- echo $challenge;
-}
-$input = json_decode(file_get_contents('php://input'), true);
-$sender = $input['entry'][0]['messaging'][0]['sender']['id'];
-$message = $input['entry'][0]['messaging'][0]['message']['text'];
-$message_to_reply = '';
-/**
- * Some Basic rules to validate incoming messages
- */
+// Replace <Subscription Key> with a valid subscription key.
+$ocpApimSubscriptionKey = '68ad895b9c5d409cb846fdcc0061f02e';
 
-/*if(preg_match('[time|current time|now]', strtolower($message))) {
-    // Make request to Time API
-    ini_set('user_agent','Mozilla/4.0 (compatible; MSIE 6.0)');
-    $result = file_get_contents("http://www.timeapi.org/utc/now?format=%25a%20%25b%20%25d%20%25I:%25M:%25S%20%25Y");
-    if($result != '') {
-        $message_to_reply = $result;
-    }
-} else {
-    $message_to_reply = 'Huh! what do you mean?';
-}*/
-$api_key="WMuixWmGz9G8FwjQFQ0lu0dzsaXxvo23";
-$url = 'https://api.mlab.com/api/1/databases/duckduck/collections/linebot?apiKey='.$api_key.'';
-$json = file_get_contents('https://api.mlab.com/api/1/databases/duckduck/collections/linebot?apiKey='.$api_key.'&q={"question":"'.$message.'"}');
-$data = json_decode($json);
-$isData=sizeof($data);
+// You must use the same location in your REST call as you used to obtain
+// your subscription keys. For example, if you obtained your subscription keys
+// from westus, replace "westcentralus" in the URL below with "westus".
+$uriBase = 'https://westcentralus.api.cognitive.microsoft.com/vision/v2.0/';
 
-if (strpos($message, 'สอนเป็ด') !== false) {
-  if (strpos($message, 'สอนเป็ด') !== false) {
-    $x_tra = str_replace("สอนเป็ด","", $message);
-    $pieces = explode("|", $x_tra);
-    $_question=str_replace("[","",$pieces[0]);
-    $_answer=str_replace("]","",$pieces[1]);
-    //Post New Data
-    $newData = json_encode(
-      array(
-        'question' => $_question,
-        'answer'=> $_answer
-      )
-    );
-    $opts = array(
-      'http' => array(
-          'method' => "POST",
-          'header' => "Content-type: application/json",
-          'content' => $newData
-       )
-    );
-    $context = stream_context_create($opts);
-    $returnValue = file_get_contents($url,false,$context);
-    $message_to_reply = 'ขอบคุณที่สอนเป็ด';
-  }
-}else{
-  if($isData >0){
-   foreach($data as $rec){
-     $message_to_reply = $rec->answer;
-   }
-  }else{
-    $message_to_reply = 'ก๊าบบ คุณสามารถสอนให้ฉลาดได้เพียงพิมพ์: สอนเป็ด[คำถาม|คำตอบ]';
-  }
+$imageUrl = 'http://upload.wikimedia.org/wikipedia/commons/3/3c/Shaki_waterfall.jpg';
+
+require_once 'HTTP/Request2.php';
+
+$request = new Http_Request2($uriBase . '/analyze');
+$url = $request->getUrl();
+
+$headers = array(
+    // Request headers
+    'Content-Type' => 'application/json',
+    'Ocp-Apim-Subscription-Key' => $ocpApimSubscriptionKey
+);
+$request->setHeader($headers);
+
+$parameters = array(
+    // Request parameters
+    'visualFeatures' => 'Categories,Description',
+    'details' => '',
+    'language' => 'en'
+);
+$url->setQueryVariables($parameters);
+
+$request->setMethod(HTTP_Request2::METHOD_POST);
+
+// Request body parameters
+$body = json_encode(array('url' => $imageUrl));
+
+// Request body
+$request->setBody($body);
+
+try
+{
+    $response = $request->send();
+    echo "<pre>" .
+        json_encode(json_decode($response->getBody()), JSON_PRETTY_PRINT) . "</pre>";
 }
-
-//API Url
-$url = 'https://graph.facebook.com/v2.6/me/messages?access_token='.$access_token;
-//Initiate cURL.
-$ch = curl_init($url);
-//The JSON data.
-$jsonData = '{
-    "recipient":{
-        "id":"'.$sender.'"
-    },
-    "message":{
-        "text":"'.$message_to_reply.'"
-    }
-}';
-//Encode the array into JSON.
-$jsonDataEncoded = $jsonData;
-//Tell cURL that we want to send a POST request.
-curl_setopt($ch, CURLOPT_POST, 1);
-//Attach our encoded JSON string to the POST fields.
-curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonDataEncoded);
-//Set the content type to application/json
-curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
-//curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/x-www-form-urlencoded'));
-//Execute the request
-if(!empty($input['entry'][0]['messaging'][0]['message'])){
-    $result = curl_exec($ch);
+catch (HttpException $ex)
+{
+    echo "<pre>" . $ex . "</pre>";
 }
 ?>
+</body>
+</html>
